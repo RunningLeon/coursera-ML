42f0a4a2720c807a7b3d1478916580df417c319b
RunningLeon <mnsheng@yeah.net>
2017-10-14 01:20:20 +0800

    Start coding with jupyter notebook

--- machine-learning-ex1/ex1/ex1.py
+++ machine-learning-ex1/ex1/ex1.py
@@ -4,18 +4,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from mpl_toolkits.mplot3d import axes3d
-
-pd.set_option('display.notebook_repr_html', False)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 150)
-pd.set_option('display.max_seq_items', None)
-
-# %config InlineBackend.figure_formats = {'pdf',}
-
 import seaborn as sns
 
-sns.set_context('notebook')
-sns.set_style('white')
-data = pd.read_table('ex1data1.txt', delimiter=',', header=0)
-print(data.shape)
-print(data.head(5))
+### Read data1
+data1 = pd.read_csv('ex1data1.txt', header=None, names=['X', 'y'])
+# data1.plot.scatter(x='X',y='y', c='r',marker='x')
+
+# print(data1.shape)
+# print(data1.head(5))
+# X = data1['X'].values
+# y = data1['y'].values
+X = np.array(data1['X'])
+y = np.array(data1['y'])
+# Scatter plot
+# plt.figure(figsize=(10, 6))
+# plt.scatter(X, y, c='r', marker='x', linewidths=1)
+#
+# plt.show()
+# plt.grid(True)
+# plt.xlabel('Profit in $10,000s')
+# plt.ylabel('Population of City in 10,000s')
+
+### compute cost function
+iterations = 1500
+alpha = 0.01
+# print(data1.shape)
+
+def computeCost(X, y, theta=None ):
+    if theta == None:
+        theta = [[0],[0]]
+    m = y.size
+    return 1/(2*m)*np.sum(np.square(X.dot(theta) - y))
+i = 0
+for x in X:
+    i += 1
+    # print("row({}): {}".format(i, x))
+
+X = np.c_(np.ones(X.shape[0]), X)
+
+print(computeCost(X, y))
