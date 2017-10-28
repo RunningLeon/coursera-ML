d70932a113b0b28c818758c9c664de6c02e5cf24
RunningLeon <mnsheng@yeah.net>
2017-10-28 18:49:52 +0800

    submit and succeed

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -13,12 +13,13 @@ Z = zeros(size(X, 1), K);
 % Instructions: Compute the projection of the data using only the top K 
 %               eigenvectors in U (first K columns). 
 %               For the i-th example X(i,:), the projection on to the k-th 
-%               eigenvector is given as follows:
+%               eigenvector is giv0en as follows:
 %                    x = X(i, :)';
 %                    projection_k = x' * U(:, k);
 %
 
-
+U_reduce = U(:,1:K);
+Z = (U_reduce'*X')';
 
 
 % =============================================================
